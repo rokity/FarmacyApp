72ab3b63963c9cd871559deb8cc8e8d0590780b1
Riccardo Amadio <riccardo.amadio1@gmail.com>
2021-04-04 16:43:11 +0200

    home dottore done

--- src/pages/Dottore/HomeDottore.js
+++ src/pages/Dottore/HomeDottore.js
@@ -1,8 +1,23 @@
 import React from "react";
 import { Card, Button, CardDeck, Jumbotron, Container, Row } from 'react-bootstrap'
+import { useHistory } from "react-router-dom";
 
 
 export default function HomeDottore() {
+    let history = useHistory();
+    function ListaPazienti(event){
+        event.preventDefault();
+        history.push("/ListaPazienti")
+    }
+    function RicetteSospese(event){
+        event.preventDefault();
+        history.push("/RicetteSospese")
+    }
+    function NuovoPazienteDottore(event){
+        event.preventDefault();
+        history.push("/NuovoPazienteDottore")
+    }
+
     return (
         <div>
             <h1>Benvenuto Dott.Riccardo</h1>
@@ -13,7 +28,7 @@ export default function HomeDottore() {
                             <Card.Header>Pazienti</Card.Header>
                             <Card.Body>
                                 <Card.Title>Lista Pazienti che usano l'app</Card.Title>
-                                <Button variant="primary">Apri</Button>
+                                <Button variant="primary" onClick={ListaPazienti}>Apri</Button>
                             </Card.Body>
                         </Card>
                         <Card className="text-center">
@@ -21,14 +36,14 @@ export default function HomeDottore() {
                             <Card.Body>
                                 <Card.Title>Lista Ricetta in Sospeso</Card.Title>
 
-                                <Button variant="primary">Apri</Button>
+                                <Button variant="primary" onClick={RicetteSospese}>Apri</Button>
                             </Card.Body>
                         </Card>
                         <Card className="text-center">
                             <Card.Header>Nuovo Paziente</Card.Header>
                             <Card.Body>
                                 <Card.Title>Aggiungi un nuovo paziente</Card.Title>
-                                <Button variant="primary">Apri</Button>
+                                <Button variant="primary" onClick={NuovoPazienteDottore}>Apri</Button>
                             </Card.Body>
                         </Card>
                     </CardDeck>
